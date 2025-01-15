7b9d223e51bc5031b33da1180bfa331a284b7162
rx-ted <1552488649@qq.com>
2025-01-15 15:39:29 +0800

    Update Middleware for lcd, malloc...

---
+++ Middlewares/Middleware.h
@@ -0,0 +1,19 @@
+#ifndef __MIDDLEWARE_H__
+#define __MIDDLEWARE_H__
+
+#ifdef __cppplusplus
+extern "C" {
+#endif
+
+#include "config.h"
+
+// use lcd ssd1306
+#ifdef MIDDLEWARE_USING_LCD_SSD1306
+
+#endif
+
+#ifdef __cppplusplus
+}
+#endif
+
+#endif
